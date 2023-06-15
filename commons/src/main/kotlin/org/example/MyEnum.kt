2c16f0f9431e50501af511c8a93c36790b7aa490
João Tarla <joao@tarla.com.br>
2023-06-15 17:55:26 +0400

    initial commit

---
+++ commons/src/main/kotlin/org/example/MyEnum.kt
@@ -0,0 +1,5 @@
+package org.example
+
+enum class MyEnum {
+    ONE, TWO, THREE
+}
---
+++ component1/src/main/kotlin/org/example/agencies/Agencies.kt
@@ -0,0 +1,8 @@
+package org.example.agencies
+
+import org.example.MyEnum
+
+fun main() {
+    val common = MyEnum.ONE
+    println("Hello World! $common")
+}
